a27e9faabade5e8cd0903e28de485cf3c20ae191
Marc Tapp <marc.tapp@gmail.com>
2020-10-11 21:59:43 -0400

    tying to get files to upload

--- art/artrouter.js
+++ art/artrouter.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
+const multer = require('multer')
 const Art = require("./artmodal.js");
 const { verifyUpload, verifyToken } = require("./artmiddleware.js");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) { 
+        cb(null, 'public')
+    },
+    filename: function (req, file, cb) { 
+        cb(null, Date.now()+ '-' +file.originalname)
+    }
+})
+const upload = multer({storage: storage}).single('file')
 
 router.get("/", (req, res) => {
   return Art.getAll().then((resp) => res.status(200).json(resp));
@@ -32,10 +43,17 @@ router.patch("/:id", verifyUpload, (req, res) => {
 });
 
 router.post("/img", (req, res) => {
-  console.log(req.body);
-  return Art.addImage(res.body)
-    .then((resp) => res.status(201).json(resp))
-    .catch(console.log);
+//   console.log(req.body);
+//   return Art.addImage(res.body)
+//     .then((resp) => res.status(201).json(resp))
+//     .catch(console.log);
+    upload(req, res, function (err) { 
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        } else if (err) { 
+            return res.status(500).json(err)
+        } else res.status(200).send(req.file)
+    })
 });
 
 router.get("/img", (req, res) => {
